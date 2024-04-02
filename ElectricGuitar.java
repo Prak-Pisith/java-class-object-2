1b10792ea0072ed919e46c4f3dbda100c4a6ca4f
The_Pi$ith <pisith@Pisiths-MBP.bbrouter>
2024-04-02 22:55:52 +0700

    implemented getter and setter methods

---
+++ ElectricGuitar.java
@@ -0,0 +1,18 @@
+class ElectricGuitar {
+    // Stats / Instance Variables
+    String brand;
+    int numOfPickups;
+    boolean rockStarUsesIt;
+
+    // Behaviors
+    // Getter & Setter
+    String getBrand() { return this.brand; }
+    void setBrand(String brand) { this.brand = brand; }
+
+    int getNumOfPickups() { return this.numOfPickups; }
+    void setNumOfPickups(int numOfPickups) { this.numOfPickups = numOfPickups; }
+
+    boolean isRockStarUsesIt() { return this.rockStarUsesIt; }
+    void setRockStarUsesIt(boolean rockStarUsesIt) { this.rockStarUsesIt = rockStarUsesIt; }
+
+}
